344c0734b21b3112ce42c7f8077b1846bf2a15fb
jesusdiez10p <jesus_diez@live.com>
2021-12-07 01:08:25 -0500

    pointers

--- app/src/main/java/pe/edu/ulima/pm/goutsidevf/MapActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/goutsidevf/MapActivity.kt
@@ -1,6 +1,7 @@
 package pe.edu.ulima.pm.goutsidevf
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -8,14 +9,21 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import pe.edu.ulima.pm.goutsidevf.Model.Event
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
-
+    private var latitud : String? = null
+    private var longitud : String? = null
+    private var nomb : String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        latitud = intent.getBundleExtra("location_data")?.getString("latitud")?.toString()
+        longitud = intent.getBundleExtra("location_data")?.getString("longitud")?.toString()
+        nomb = intent.getBundleExtra("location_data")?.getString("nombre")?.toString()
         setContentView(R.layout.activity_map)
+
         createMapFragment()
     }
 
@@ -32,15 +40,28 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private fun createMarkers() {
 
         val cooULIMA = LatLng(-12.084847, -76.973097)
-        val mkULIMA = MarkerOptions().position(cooULIMA).title("Universidad de Lima")
+        val nombre = "Universidad de Lima"
+        val mkULIMA = MarkerOptions().position(cooULIMA).title(nombre)
 
         //Añadir Marcador de la Universidad de Lima
         map.addMarker(mkULIMA)
 
+        if(nomb!=null){
+            val markLocation = MarkerOptions()
+                .position(LatLng(latitud!!.toDouble(), longitud!!.toDouble()))
+                .title("Evento: ${nomb}")
+            map.addMarker(markLocation)
+        }
         //Que haga un Zoom al Marcador de la ULIMA
+        if(latitud==null&&longitud==null){
         map.animateCamera(
             CameraUpdateFactory.newLatLngZoom(cooULIMA, 18f), 4000, null
-        )
+        )}else{
+            map.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(LatLng(latitud!!.toDouble(),longitud!!.toDouble())
+                    , 14f), 4000, null
+            )
+        }
 
     }
 
--- app/src/main/java/pe/edu/ulima/pm/goutsidevf/Model/Event.kt
+++ app/src/main/java/pe/edu/ulima/pm/goutsidevf/Model/Event.kt
@@ -4,17 +4,16 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+
 data class Event(
-    @PrimaryKey()
-    @ColumnInfo(name="nombre")
+
     val name: String,
-    @ColumnInfo(name="fecha")
+
     val Date : Long,
-    @ColumnInfo(name="image")
+
     val image : String,
-    @ColumnInfo(name="latitud")
+
     val latitud : Double,
-    @ColumnInfo(name="longitud")
+    
     val longitud: Double
 )
